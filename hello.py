ce4c11df79a6a41b151b03a49ded06d91e6c4b5b
xinxin hu <cynthiahu3727@gmail.com>
2023-09-18 17:34:49 -0400

    complete example 4-7

--- hello.py
+++ hello.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired,Email
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a string nobody knows'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-@app.route('/')
+
+class NameForm (FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()]) # 'what is your name' is used when rendering form to HTML
+    submit = SubmitField('Submit')
+@app.route('/', methods=['GET', 'POST']) #when methods not specified, takes 'GET' method only
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name != None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ' '
+        return redirect(url_for('index')) # url_for takes the endpoint name as required argument, by default is the name of the view function attached to the route
+    return render_template('index.html',form=form, name=session.get('name'))
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name, current_time=datetime.utcnow())
