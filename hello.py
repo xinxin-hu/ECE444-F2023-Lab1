646fd16c7bc8571b7376fb2cc4a2043ae65bfaba
xinxin hu <cynthiahu3727@gmail.com>
2023-09-18 10:58:43 -0400

    complete activity 3: replay and modify example in chapter 3

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 @app.route('/')
 def index():
-    return '<h1> Hello World! </h1>'
+    return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hello, {}! </h1>'.format(name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ =='__main__':
     app.run()
